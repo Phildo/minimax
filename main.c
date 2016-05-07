d1fe5ebbe56654902a96557826db00fa991b5ebc
Phildo <phildo211@gmail.com>
2016-05-07 11:16:57 -0500

    basic moves finder

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "stdlib.h"
 
 #define BOARD_S 4
 
@@ -17,9 +18,28 @@ void initBoard(board *b)
   {
     for(int x = 0; x < BOARD_S; x++)
     {
-      b->pos[i(x,y)] = (y%2+x)%2;
+      if(y == 0)
+        b->pos[i(x,y)] = (y%2+x)%2;
+      else if(y == BOARD_S-1)
+        b->pos[i(x,y)] = ((y%2+x)%2)*2;
+      else
+        b->pos[i(x,y)] = 0;
     }
   }
+}
+
+void copyBoardSwap(board *from, int x0, int y0, int x1, int y1, board *to)
+{
+  for(int y = 0; y < BOARD_S; y++)
+  {
+    for(int x = 0; x < BOARD_S; x++)
+    {
+      to->pos[i(x,y)] = from->pos[i(x,y)];
+    }
+  }
+  int tmp = to->pos[i(x0,y0)];
+  to->pos[i(x0,y0)] = to->pos[i(x1,y1)];
+  to->pos[i(x1,y1)] = tmp;
 }
 
 void printBoard(board *b)
@@ -28,10 +48,54 @@ void printBoard(board *b)
   {
     for(int x = 0; x < BOARD_S; x++)
     {
-      printf("%i",b->pos[i(x,y)]);
+      switch(b->pos[i(x,y)])
+      {
+        case 0: printf("."); break;
+        case 1: printf("x"); break;
+        case 2: printf("o"); break;
+      }
     }
     printf("\n");
   }
+}
+
+void possibleMoves(board *b, int player, board **moves, int *n_moves)
+{
+  board *boards = (board *)malloc(sizeof(board)*100);
+  int boards_i = 0;
+  for(int y = 0; y < BOARD_S; y++)
+  {
+    for(int x = 0; x < BOARD_S; x++)
+    {
+      if(b->pos[i(x,y)] == player)
+      {
+        if(player == 1)
+        {
+          if(y+1 < BOARD_S)
+          {
+            if(x+1 < BOARD_S && b->pos[i(x+1,y+1)] == 0) { copyBoardSwap(b,x,y,x+1,y+1,&boards[boards_i]); boards_i++; }
+            if(x-1 >= 0      && b->pos[i(x-1,y+1)] == 0) { copyBoardSwap(b,x,y,x-1,y+1,&boards[boards_i]); boards_i++; }
+          }
+        }
+        if(player == 2)
+        {
+          if(y-1 >= 0)
+          {
+            if(x+1 < BOARD_S && b->pos[i(x+1,y-1)] == 0) { copyBoardSwap(b,x,y,x+1,y-1,&boards[boards_i]); boards_i++; }
+            if(x-1 >= 0      && b->pos[i(x-1,y-1)] == 0) { copyBoardSwap(b,x,y,x-1,y-1,&boards[boards_i]); boards_i++; }
+          }
+        }
+      }
+    }
+  }
+
+  board *trimmed_boards = (board *)malloc(sizeof(board)*boards_i);
+  for(int i = 0; i < boards_i; i++)
+    trimmed_boards[i] = boards[i];
+  free(boards);
+
+  *moves = trimmed_boards;
+  *n_moves = boards_i;
 }
 
 int main()
@@ -39,5 +103,15 @@ int main()
   board b;
   initBoard(&b);
   printBoard(&b);
+
+  board *moves;
+  int n_moves;
+  possibleMoves(&b, 1, &moves, &n_moves);
+
+  for(int i = 0; i < n_moves; i++)
+  {
+    printf("Move %d:\n",i);
+    printBoard(&moves[i]);
+  }
 }
 
